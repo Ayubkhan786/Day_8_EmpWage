3d7bc3295bf07d392cf53153833988e4ec93197a
your Name <mohamedabdulayubm@gmail.com>
2022-06-21 12:31:27 +0530

    Part

--- Day_8 Emp/Program.cs
+++ Day_8 Emp/Program.cs
@@ -1,10 +1,14 @@
 ﻿
 Console.WriteLine("Welcome to Employee Wage Computation Program");
+int empAbsent = 0;
 int empPresent = 1;
+int empPartTime = 2;
+
 int empWrkHrs = 0;
 int empSalary = 50;
+
 Random num = new Random();
-int attendance = num.Next(0, 2);
+int attendance = num.Next(0, 3);
 if (attendance == empPresent)
 {
     Console.WriteLine("Is Present");
@@ -12,12 +16,18 @@ if (attendance == empPresent)
     empSalary = 50;
 }
 
+else if (attendance == empAbsent)
+    {
+        Console.WriteLine("Is Absent");
+        empWrkHrs = 0;
+        empSalary = 0;
+    }
 else
-{
-    Console.WriteLine("Is Absent");
-    empWrkHrs = 0;
-    empSalary = 0;
-}
+    {
+        Console.WriteLine("Is PartTime");
+        empWrkHrs = 4;
+        empSalary = 50;
+    }
 
 int empWage = (empWrkHrs * empSalary);
 Console.WriteLine("The total salary is :" + empWage);
